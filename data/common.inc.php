d01119292f5d94d22acba4db1c820a59d1e36b7f
moheqionglin <zhouwanli2009@yeah.net>
2017-10-15 21:28:15 +0800

    first commit

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'suimei_site';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'mohe!@#';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
